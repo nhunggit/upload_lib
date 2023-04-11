ebf093343880d8ff42cca0b52b650eb8b29805c2
Nhungltk <nhungltk@bkav.com>
2023-04-11 14:10:43 +0700

    create class test in lib

--- app/src/main/java/com/bkav/upload_lib/MainActivity.java
+++ app/src/main/java/com/bkav/upload_lib/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.bkav.libtest.TestClassInLib;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
